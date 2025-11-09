8cf6e941c71ba8826b2aa29b99770637bbab5707
yungiy <devyungiy@gmail.com>
2025-11-09 21:43:18 +0900

    .
    .

--- ordly-admin/src/features/auth/components/auth.tsx
+++ ordly-admin/src/features/auth/components/auth.tsx
@@ -5,7 +5,7 @@ export default function Auth() {
     <div className='flex min-h-screen flex-col items-center justify-center bg-gray-100'>
       <div className='w-full max-w-md rounded-lg bg-white p-8'>
         <h1 className='mb-6 text-center text-3xl font-bold text-gray-800'>
-          Ordly Admin
+         Table Admin
         </h1>
         <AuthForm />
       </div>
